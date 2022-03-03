5641db887fed97804ddd0e82539f4ebcfaf88f1f
affigueir <affigueir@gmail.com>
2022-03-03 12:47:20 +0000

    SkynetPatternSwitch Added

---
+++ Aula_02/SkynetPatternSwitch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SkynetPatternSwitch
+{
+    class Program
+    {
+        // Variáveis auxiliares
+       
+        static void Main(string[] args)
+        {
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+           string question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            string response = question switch
+            {
+                "How are you?" => "I'm fine, thank you",
+                "What's your name?" => "Skynet",
+                "What's your mission?" => "Destroy mankind!",
+                _ => "You got me, I'm not THAT smart!",
+            };
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+
+
+        }
+    }
+}
