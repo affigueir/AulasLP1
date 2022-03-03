2ab0febc6c8d04e2946c63dbf730976f118e5d35
affigueir <affigueir@gmail.com>
2022-03-03 11:42:43 +0000

    Incdec added

---
+++ Aula_02/IncDec/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int i = 1;
+            int b = 2;
+            Console.WriteLine($"i = {i} b = {b}");
+            Console.WriteLine("Run IncDec?");
+            Console.ReadLine();
+            Console.WriteLine($"As you can see b is now {++b} while i is still {i++}");
+        }
+    }
+}
