881af29824db7e3e25c84cd1a8dd6bf3936b4727
affigueir <affigueir@gmail.com>
2022-03-10 11:25:45 +0000

    PercorreString working

---
+++ Aula_03/PercorreString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert phrase: ");
+            string insert = Console.ReadLine();
+
+            Console.Write("Insert a char: ");
+            char a = char.Parse(Console.ReadLine());
+            Console.Write("Your new string is ");
+
+            for (int i = 0; i < insert.Length; i++)
+            {
+                if (insert[i] != a)
+                {
+                    Console.Write(insert[i]);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
